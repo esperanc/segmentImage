04236b3a407031ceb0e5fb57540f7324ff906f76
Claudio Esperança <claudio.esperanca@gmail.com>
2017-01-17 11:03:57 -0200

    Many enhancements to the segmenter

--- segmenter.js
+++ segmenter.js
@@ -75,7 +75,7 @@ var RasterGraph = function (w,h) {
 	};
 
 	// Returns a list of connected components of this graph. 
-	// Each connected component is a list of addresses of m
+	// Each connected component is an array of addresses (indices) of m
 	self.connectedComponents = function () {
 		var flag = new Int8Array(n);
 		for (var i = 0; i < n; i++) flag[i]=1;
@@ -110,103 +110,207 @@ var RasterGraph = function (w,h) {
 			if (flag[i]) comps.push (visit(i));
 		}
 		return comps;
-	}
-
-	// Given an ImageData object with same dimensions as this graph,
-	// returns an array of ImageData objects, each with a connected 
-	// component of the original image. Each ImageData result is also
-	// augmented with fields x0 and y0 which contain the offset of the 
-	// upper left corner of the result w.r.t. original.
-	self.segmentedImages = function (imgdata) {
-		
-		var width = imgdata.width, height = imgdata.height;
-		console.assert (width == w && height == h);
-		var src = new Uint32Array (imgdata.data.buffer);
-
-		function segmentComponent (comp) {
-			// find bounding box
-			var x0 = comp[0] % w, x1 = x0;
-			var y0 = ~~(comp[1] / w), y1 = y0;
-			for (let xy of comp) {
-				var x = xy % w;
-				var y = ~~(xy / w);
-				if (x < x0) x0=x;
-				if (x > x1) x1=x;
-				if (y < y0) y0=y;
-				if (y > y1) y1=y;
-			}
-			// Create new ImageData
-			var sw = x1-x0+1, sh = y1-y0+1, ns = sh*sh;
-			var canvas = document.createElement('canvas');
-			canvas.setAttribute ('width', sw);
-			canvas.setAttribute ('height', sh);
-			var ctx = canvas.getContext('2d');
-			var seg = ctx.getImageData(0,0,sw,sh);
-			var dst = new Uint32Array (seg.data.buffer);
-			// Fill dst with pixels from src
-			for (let xy of comp) {
-				var x = xy % w - x0;
-				var y = ~~(xy / w) - y0;
-				var dxy = y * sw + x;
-				dst[dxy] = src[xy];
-			}
-			ctx.putImageData (seg,0,0);
-			canvas.x0 = x0;
-			canvas.y0 = y0;
-			return canvas;
-		}
-
-		var result = [];
-		for (let comp of self.connectedComponents()) {
-			result.push (segmentComponent(comp));
-		}
-		return result;
-	}
+	};
 
 	return self;
 };
+
+// Represents a connected component (segment) of imgdata (an canvasImageData object)
+// Comp is an array of pixel indices by sampling.
+// pixels from imgdata. Pclass is a function that takes
+// r,g,b,a as an argument and returns the segmentation class for the first pixel
+// in the component (in theory, all pixels of the component have the same class).
+function Segment (comp, imgdata, pclass) {
+
+	var data = imgdata.data;
+	var dataArray = new Uint32Array (data.buffer);
+	var w = imgdata.width;
+	var avgColor = [0,0,0,0];
+	var colorFreq = {};
+	var mostFreq = null;
+	var topFreq = 0;
+	var xy = comp [0];
+	var i = xy*4;
+	var r = data[i], g = data[i+1], b = data[i+2], a = data[i+3];
+	this.pClass = pclass;
+	var minColor = [r,g,b,a];
+	var maxColor = [r,g,b,a];
+	var x0 = xy % w, x1 = x0;
+	var y0 = ~~(xy / w), y1 = y0;
+	for (let xy of comp) {
+		var x = xy % w;
+		var y = ~~(xy / w);
+		if (x < x0) x0=x;
+		if (x > x1) x1=x;
+		if (y < y0) y0=y;
+		if (y > y1) y1=y;
+		var i = xy*4;
+		var color = dataArray[xy];
+		if (color in colorFreq) colorFreq[color]++;
+		else colorFreq[color] = 1;
+		if (colorFreq[color] > topFreq) {
+			topFreq = colorFreq[color];
+			mostFreq = xy;
+		}
+		for (var k = 0; k < 4; k++) {
+			var val = data[i+k];
+			minColor [k] = Math.min(val,minColor[i]);
+			maxColor [k] = Math.max(val,maxColor[i]);
+			avgColor [k] += val;
+		}
+	}
+	var n = comp.length;
+	this.avgColor = [~~(avgColor[0]/n), ~~(avgColor[1]/n), ~~(avgColor[2]/n), ~~(avgColor[3]/n)],
+	this.minColor = minColor;
+	this.maxColor = maxColor;
+	//this.segmentClass = pclass(this.avgColor[0],this.avgColor[1],this.avgColor[2],this.avgColor[3]);
+	i = mostFreq*4;
+	this.segmentClass = pclass(data[i], data[i+1], data[i+2], data[i+3]);
+	this.comp = comp;
+	this.imgdata = imgdata;
+	this.x = x0;
+	this.y = y0;
+	this.width = x1-x0+1;
+	this.height = y1-y0+1;
+	this.compCount = 1;
+}
+
+// Returns a canvas(image) for the component. The canvas object is
+// augmented with fields x0 and y0 which contain the offset of the 
+// upper left corner of the result w.r.t. original imagedata.
+Segment.prototype.canvas = function () {
+	var sw = this.width, sh = this.height, ns = sh*sh;
+	var x0 = this.x, y0 = this.y;
+	var canvas = document.createElement('canvas');
+	canvas.setAttribute ('width', sw);
+	canvas.setAttribute ('height', sh);
+	var ctx = canvas.getContext('2d');
+	var seg = ctx.getImageData(0,0,sw,sh);
+	var src = new Uint32Array (this.imgdata.data.buffer);
+	var dst = new Uint32Array (seg.data.buffer);
+	var w = this.imgdata.width;
+	// Fill dst with pixels from src
+	for (let xy of this.comp) {
+		var x = xy % w - x0;
+		var y = ~~(xy / w) - y0;
+		var dxy = y * sw + x;
+		dst[dxy] = src[xy];
+	}
+	ctx.putImageData (seg,0,0);
+	canvas.x0 = x0;
+	canvas.y0 = y0;
+	return canvas;
+} 
+
+// Merges the info of component other into this one
+Segment.prototype.merge = function (other) {
+	console.assert (this.imgdata == other.imgdata);
+	var x0 = Math.min(this.x,other.x);
+	var y0 = Math.min(this.y,other.y);
+	var x1 = Math.max(this.x+this.width,other.x+other.width);
+	var y1 = Math.max(this.y+this.height,other.y+other.height);
+	this.x = x0;
+	this.y = y0;
+	this.width = x1-x0;
+	this.height = y1-y0;
+	var n1 = this.comp.length;
+	var n2 = other.comp.length;
+	for (var i = 0; i < 4; i++) {
+		this.avgColor [i] = ~~((this.avgColor[i]*n1 + other.avgColor[i]*n2)/(n1+n2));
+		this.minColor [i] = Math.min (this.minColor[i], other.minColor[i]);
+		this.maxColor [i] = Math.min (this.maxColor[i], other.maxColor[i]);
+	}
+	var dst = this.comp;
+	var src = other.comp;
+	for (let c of src) {
+		dst.push (c);
+	}
+	this.compCount += other.compCount;
+}
+
+// Returns an object map, such that map[segClass] is the number of
+// 4-connected neighbors of the segment that have the given segment class
+Segment.prototype.neighborCount = function () {
+
+	var map = {};
+	var w = this.imgdata.width;
+	var h = this.imgdata.height;
+	var neighbor = new Set();
+
+	// Fill dst with pixels from src
+	for (let xy of this.comp) {
+		neighbor.add(xy);
+		var x = xy % w;
+		var y = ~~(xy / w);
+		if (x > 0) neighbor.add(xy-1);
+		if (x < w-1) neighbor.add(xy+1);
+		if (xy >= w) neighbor.add(xy-w);
+		if (y < h-1) neighbor.add(xy+w);
+	}
+	var data = this.imgdata.data;
+	for (let xy of neighbor) {
+		var i = xy*4;
+		var c = this.pClass (data[i],data[i+1],data[i+2],data[i+3]);
+		if (c in map) map[c]++;
+		else map[c]=1;
+	}
+	return map;
+}
 
 // Segments an ImageData object imgdata. Function pclass classifies
 // a pixel into a segment class -- its argument list are the color 
 // components r,g,b,a and it should
 // return an integer or some other value that can be tested for equality
-// with other segment classes. The function returns a RasterGraph object
-// where edges between pixels with different classes have been cut
-var segmenter = function (imgdata, pclass) {
+// with other segment classes. 
+//
+// If specified, diff should be a predicate that tells whether two colors
+// belong to different classes. If not specified, diff (r1,g1,b1,a1,r2,g2,b2,a2)
+// is set to pclass(r1,g1,b1,a1)!=pclass(r2,g2,b2,a2).
+//
+// The function returns an array of Segments.
+var segmenter = function (imgdata, pclass, diff) {
 
 	var width = imgdata.width, height = imgdata.height, data = imgdata.data;
 
 	var rg = new RasterGraph (width, height);
+
+	diff = diff || function (r1,g1,b1,a1,r2,g2,b2,a2) { return pclass(r1,g1,b1,a1) != pclass(r2,g2,b2,a2); }
+
 	for (var px = 0; px+1 < width; px++) {
 		for (var py = 0; py+1 < height; py++) {
 			var i = (py*width+px)*4;
-			var class1 = pclass (data[i],data[i+1],data[i+2],data[i+3]);
 			var j = i+width*4;
-			var class2 = pclass (data[j],data[j+1],data[j+2],data[j+3]);
-			if (class1 != class2) rg.cutSouthEdge (px,py);
+			if (diff (data[i],data[i+1],data[i+2],data[i+3],data[j],data[j+1],data[j+2],data[j+3])) {
+				rg.cutSouthEdge (px,py);
+			}
 			j = i+4;
-			class2 = pclass (data[j],data[j+1],data[j+2],data[j+3]);
-			if (class1 != class2) rg.cutEastEdge (px,py);
+			if (diff (data[i],data[i+1],data[i+2],data[i+3],data[j],data[j+1],data[j+2],data[j+3])) {
+				rg.cutEastEdge (px,py);
+			}
 		}
 	}
 
 	var py = height-1;
-	for (var px = 0; px < width; px++) {
+	for (var px = 0; px+1 < width; px++) {
 		var i = (py*width+px)*4;
-		var class1 = pclass (data[i],data[i+1],data[i+2],data[i+3]);
 		var j = i+4;
-		var class2 = pclass (data[j],data[j+1],data[j+2],data[j+3]);
-		if (class1 != class2) rg.cutEastEdge (px,py);
+		if (diff (data[i],data[i+1],data[i+2],data[i+3],data[j],data[j+1],data[j+2],data[j+3])) {
+			rg.cutEastEdge (px,py);
+		}
 	}
 
 	var px = width-1;
 	for (var py = 0; py+1 < height; py++) {
 		var i = (py*width+px)*4;
-		var class1 = pclass (data[i],data[i+1],data[i+2],data[i+3]);
 		var j = i+width*4;
-		var class2 = pclass (data[j],data[j+1],data[j+2],data[j+3]);
-		if (class1 != class2) rg.cutSouthEdge (px,py);
+		if (diff (data[i],data[i+1],data[i+2],data[i+3],data[j],data[j+1],data[j+2],data[j+3])) {
+			rg.cutSouthEdge (px,py);
+		}
 	}
 
-	return rg;
+	var result = [];
+	for (let comp of rg.connectedComponents()) {
+		result.push (new Segment (comp, imgdata, pclass));
+	}
+	return result;
 }
--- sketch.js
+++ sketch.js
@@ -35,16 +35,269 @@ var settings = {
 	uploadImage: function () {
         imageLoader.click();
     },
+    segmentMethod : 'palette',
+	background : "#ffffff",
+	bkgTolerance : 5, 
+    segment : function () {
+    	segment()
+    },
+    colors : 16,
+    maxColorDist : 128,
+    reduceWithinDistance : reduceWithinDistance,
+    reduceInconditional : reduceInconditional,
+    layout : 'left-right',
+    'shrink-to-fit' : true,
+
 };
 
 function setupGui() {
     var gui = new dat.GUI();
     gui.add (settings, 'uploadImage');
+    gui.add (settings, 'colors', 1, 256);
+    gui.add (settings, 'segmentMethod', ['palette', 'background'])
+    gui.addColor (settings, 'background');
+    gui.add (settings, 'bkgTolerance', 0, 255);
+    gui.add (settings, 'segment');
+    gui.add (settings, 'reduceInconditional');
+    gui.add (settings, 'maxColorDist', 1, 256);
+    gui.add (settings, 'reduceWithinDistance');
+    gui.add (settings, 'layout', ['left-right','top-bottom']);
+    gui.add (settings, 'shrink-to-fit');
+
+    // Avoid events on the gui to be passed to the canvas below
+    var stop = function (e) {
+        e.stopPropagation();
+    }
+    var events = ['mousedown', 'keypress', 'keydown'];
+    var domElement = document.getElementsByTagName("div")[0];
+    events.forEach (function (e) {
+        domElement.addEventListener (e, stop, false);
+    });
+    //===
 }
 
-var img, segs, iseg;
+var img, imgData, segs, iseg, pal, cutMatrix;
 var canvas;
 var context;
+
+//
+// Given two quadruples rgba, returns their euclidean distance
+//
+function colorDist (a,b) {
+	var d = [a[0]-b[0],a[1]-b[1],a[2]-b[2],a[3]-b[3]];
+	return Math.sqrt(d[0]*d[0]+d[1]*d[1]+d[2]*d[2]+d[3]*d[3]);
+}
+
+//
+// Removes undefined elements of an array
+//
+function compressArray(a) {
+	for (var i = 0; i < a.length; i++) {
+		while (i < a.length && a[i] === undefined) a.splice(i,1);
+	}
+}
+
+// 
+// Given an array of segments segs, returns an array of pairs (i,j)
+// of segment classes that could be merged. 
+//
+// Function mergeable (internalCount, externalCount, bestNeighborCount, bestNeighborColorDist)
+// should return true if a given segment should be merged with its best neighbor segment, where
+// internalCount is the number of internal neighbors (neighbor pixels in the same segment), externalCount
+// is the number of external neighbors (neighbor pixels in any different segment), bestNeighborCount
+// is the number of neighbor pixels in the most popular other segment and bestNeighborColorDist is
+// the euclidean distance between the average color of the segment and its most popular other
+// segment.
+//
+function mergeablePairs (segs, mergeable) {
+	// maps each segment segmentClass to the index in segs where they are
+	var index = [];
+	for (var i = 0; i < segs.length; i++) {
+		var c = ~~(segs[i].segmentClass);
+		console.assert (index[c] == undefined);
+		index[c] = i;
+	}
+	var pairs = [];
+	for (let seg of segs) {
+		var neighbors = seg.neighborCount();
+		var i = seg.segmentClass;
+		var maxcount = 0; // Count of neighbor with largest count
+		var sum = 0;      // Total neighbor count
+		var jmax = -1;    
+		for (var j in neighbors) {
+			var n = neighbors[j];
+			j = ~~j;
+			if (j != i) {
+				if (n > maxcount && index[j]) {
+					jmax = j;
+					maxcount = n;
+				}
+				sum += n;
+			}
+		}
+		if (jmax == -1) continue;
+		console.log (neighbors);
+		console.log (i,index[i],jmax,index[jmax]);
+		var dist = colorDist(segs[index[i]].avgColor,segs[index[jmax]].avgColor);
+		console.log (sum, dist);
+		if (mergeable (neighbors[i], sum, maxcount, dist)) {
+			pairs.push ({ i:i, j:jmax });
+		}
+	}
+	return pairs;
+}
+
+//
+// Given an array of segments segs and an array of pairs candidates (i,j) of indices
+// for segs, merge all segments segs[j] with segs[i]
+//
+function reduceSegments (segs, candidates) {
+	// maps each segment segmentClass to the index in segs where they are
+	var index = [];
+	// maps each segment class to the final merged class
+	var sClass = {};
+
+	for (var i = 0; i < segs.length; i++) {
+		var c = ~~(segs[i].segmentClass);
+		sClass[c] = c;
+		console.assert (index[c] == undefined);
+		index[c] = i;
+	}
+
+	// Merge all pairs of segment classes
+	for (let pair of candidates) {
+		var i = ~~pair.i; 
+		while (~~sClass[i] != i) i = ~~sClass[i];
+		var j = ~~pair.j;
+		while (~~sClass[j] != j) j = ~~sClass[j];
+		if (i != j)	{
+			console.log ("Merging classes", ~~pair.i, i, "and ", ~~pair.j, j);
+			segs[index[i]].merge(segs[index[j]]);
+			segs[index[j]] = undefined;
+			sClass[j] = i;
+			sClass[pair.j] = i;
+			sClass[pair.i] = i;
+			index[j] = index[i];
+		}
+	}
+
+	// Find the final class of each original segment class
+	var newClass = {};
+	for (var c in sClass) {
+		var i = ~~c;
+		while (~~sClass[i] != i) i = ~~sClass[i];
+		newClass[c]=i;
+	}
+	sClass = newClass;
+
+	compressArray (segs);
+
+	for (var i = 0; i < segs.length; i++) {
+		var seg = segs[i];
+		var orig = seg.pClass;
+		var pClass = function (r,g,b,a) {
+			var c = orig(r,g,b,a);
+			return sClass[c];
+		}
+		seg.pClass = pClass;
+	}
+	console.log (segs);
+}
+
+//
+// Performs the segmentation of the loaded image (imgData)
+//
+function segment () {
+	// Color quantize
+	var opts = { colors : settings.colors };
+	quant = new RgbQuant(opts);
+	quant.sample (imgData);
+	pal = quant.palette();
+	
+	var rgba = new Uint8Array(4);
+	var rgba32 = new Uint32Array (rgba.buffer);
+
+	// Classifies a pixel according to the quantizer palette
+	var classifier = function (r,g,b,a) {
+		rgba[0]=r; rgba[1] = g; rgba [2] = b; rgba[3] = a;
+		var segClass = quant.nearestIndex(rgba32[0]);
+		if (segClass == null) return settings.colors;
+		return segClass;
+	};
+	
+	// Classifies two pixels as belonging to different segments
+	// according to whether one is background and the other is not
+	var bkgColor = color (settings.background);
+	bkg = [~~red(bkgColor),
+	    	   ~~green(bkgColor),
+	           ~~blue(bkgColor), 0];
+	count1 = 0;
+	count2 = 0;
+	backgroundDiff = function (r1,g1,b1,a1,r2,g2,b2,a2) {
+		var result =  (colorDist ([r1,g1,b1,0],bkg) <= settings.bkgTolerance) != 
+			   (colorDist ([r2,g2,b2,0],bkg) <= settings.bkgTolerance);
+	    if (result) count1++;
+	    else count2++;
+	    return result;
+	}
+
+	var diff = settings.segmentMethod == 'palette' ? undefined : backgroundDiff;
+
+	// var classifier = function (r,g,b,a) {
+	// 	return a == 0 || (r + g + b >= 128*3); 
+	// };
+
+	segs = segmenter (imgData, classifier, diff);
+
+	// Merge all components with the same classification
+	var merged = new Array (settings.colors);
+	console.log (segs.length + " segments originally");
+	for (let seg of segs) {
+		var i = seg.segmentClass;
+		if (merged[i]) merged[i].merge(seg);
+		else merged[i] = seg;
+	}
+	segs = merged;
+	compressArray(segs);
+	console.log (segs);
+	segmentsToImages();
+}
+
+
+// Merge segments that look like they contain scattered pixels from some 
+// antialiased object with the segment closest in color, provided their
+// average color are sufficiently similar.
+function reduceWithinDistance() {
+	var pairs = mergeablePairs(segs, function (internal, external, best, dist) {
+		return (dist < settings.maxColorDist && external > internal);
+	});
+	console.log (pairs);
+	reduceSegments (segs, pairs);
+	segmentsToImages();
+}
+
+// Merge segments that look like they contain scattered pixels from some 
+// antialiased object with the segment closest in color.
+function reduceInconditional () {
+	var pairs = mergeablePairs(segs, function (internal, external, best, dist) {
+		return (external > internal);
+	});
+	console.log ("new pairs", pairs);
+	reduceSegments (segs, pairs);
+	segmentsToImages();
+}
+
+//
+// Computes images for all segments in segs
+//
+function segmentsToImages () {
+
+	for (let seg of segs) {
+		seg.img = seg.canvas();
+	}
+	iseg = 0;
+}
+
 
 function mousePressed () {
 	if (segs) {
@@ -54,45 +307,59 @@ function mousePressed () {
 }
 
 function setup() {
-	canvas = createCanvas (600,400).canvas;
+	canvas = createCanvas (1000,800).canvas;
 	context = canvas.getContext('2d');
 	setupGui();
 	imageLoaderCallback = function (imgElement) {
 		img = imgElement;
-		var data = getImageData (img);
-
-		// Color quantize
-		var opts = { colors : 4 };
-		var quant = new RgbQuant(opts);
-		quant.sample (data);
-		var pal = quant.palette();
-		//var reduce = quant.reduce (data,2);
-
-		var rgba = new Uint8Array(4);
-		var rgba32 = new Uint32Array (rgba.buffer);
-
-		var quantClassifier = function (r,g,b,a) {
-			rgba[0]=r; rgba[1] = g; rgba [2] = b; rgba[3] = a;
-			return quant.nearestColor(rgba32);
-		};
-		
-		var classifier = function (r,g,b,a) {
-			return a == 0 || (r + g + b >= 128*3); 
-		};
-
-		var rg = segmenter (data, quantClassifier);
-		var comps = rg.connectedComponents();
-		segs = rg.segmentedImages(data);
-		iseg = 0;
-		console.log (segs);
+		imgData = getImageData (img);
+		segment ();
 	}
 }
 
+var tmp;
+
 function draw() {
-	background (200);
+	context.fillStyle = settings.background;
+	context.fillRect(0,0,width,height);
 	if (img) {
-		context.drawImage (img, 0,0);
+		//context.drawImage (img, 0,0);
 		var seg = segs[iseg];
-		context.drawImage (seg, img.width + seg.x0, seg.y0);
+		var s = 40, scale = 1.0;
+		var sx = img.width, sy = 0, px = 0, py = img.height;
+		if (settings.layout != 'left-right') {
+			sx = 0;
+			sy = img.height;
+			py = img.height*2;
+			if (py + s > height) {
+				scale = (height - s) / py;
+			}
+		}
+		else {
+			if (img.width * 2 > width) {
+				scale = width / img.width / 2;
+			}
+		}
+		context.drawImage (img, 0, 0, img.width*scale, img.height*scale);
+		context.drawImage (seg.img, (sx+seg.x)*scale, (sy+seg.y)*scale, seg.width*scale, seg.height*scale);
+
+		var x = px*scale, y = py*scale;
+		for (var i = 0; i < pal.length; i+=4) {
+			fill (pal[i],pal[i+1],pal[i+2]);
+			if (segs[iseg].segmentClass == i/4) {
+				var c = segs[iseg].avgColor;
+				stroke (c[0],c[1],c[2]);
+				strokeWeight(s/4);
+			}
+			else {
+				noStroke();
+			}
+			rect (x,y,s,s);
+			x += s;
+			if (x > img.width) {
+				x = px;
+				y += s;
+			}
+		}
 	}
 }
